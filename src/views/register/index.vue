0eba682ff9232681391c0ce2764754c76988934d
lyy <1528759333@qq.com>
2023-07-08 11:50:10 +0800

    04_表单的校验

--- src/views/register/index.vue
+++ src/views/register/index.vue
@@ -6,18 +6,23 @@
       <!-- 标题的盒子 -->
       <div class="title-box"></div>
       <!-- 注册的表单区域 -->
-      <el-form ref="form" :model="form">
-        <el-form-item>
+      <el-form ref="form" :model="form" :rules="rulesObj">
+        <el-form-item prop="username">
           <el-input
             placeholder="请输入用户名"
             v-model="form.username"
           ></el-input>
         </el-form-item>
-        <el-form-item>
-          <el-input placeholder="请输入密码" v-model="form.password"></el-input>
-        </el-form-item>
-        <el-form-item>
+        <el-form-item prop="password">
           <el-input
+            type="password"
+            placeholder="请输入密码"
+            v-model="form.password"
+          ></el-input>
+        </el-form-item>
+        <el-form-item prop="repassword">
+          <el-input
+            type="password"
             placeholder="请再次确认密码"
             v-model="form.repassword"
           ></el-input>
@@ -37,11 +42,43 @@
 export default {
   name: 'my-register',
   data () {
+    const samePwdFn = (rule, value, callback) => {
+      if (value !== this.form.password) {
+        callback(new Error('两次输入的密码不一致'))
+      } else {
+        callback()
+      }
+    }
     return {
       form: {
         username: '',
         password: '',
         repassword: ''
+      },
+      rulesObj: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          {
+            pattern: /^[a-zA-Z0-9]{1,10}$/,
+            message: '用户名必须是1-10位的大小写字母数字组成',
+            trigger: 'blur'
+          }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          {
+            pattern: /^\S{6,15}$/,
+            message: '密码必须是6-15位非空字符组成',
+            trigger: 'blur'
+          }
+        ],
+        repassword: [
+          { required: true, message: '请再次输入密码', tigger: 'blur' },
+          {
+            validator: samePwdFn,
+            tigger: 'blur'
+          }
+        ]
       }
     }
   },
