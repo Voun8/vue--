cef6d0f018a01c501deb6c5cf49421659e3964ea
lyy <1528759333@qq.com>
2023-07-10 18:58:19 +0800

    20_权限拦截控制

--- src/router/index.js
+++ src/router/index.js
@@ -23,11 +23,24 @@ const routes = [
 const router = new VueRouter({
   routes
 })
+const whiteList = ['/login', '/reg']
+// 全局前置路由守卫
+// 浏览器第一次打开项目页面，会触发一次全局前置路由守卫函数
 router.beforeEach((to, from, next) => {
   const token = store.state.token
-  if (token && !store.state.userInfo.username) {
-    store.dispatch('getUserInfoActions')
+  if (token) {
+    if (!store.state.userInfo.username) {
+      store.dispatch('getUserInfoActions')
+    }
+    next()
+  } else {
+    // 未登录
+    // 强制切换到登录路径
+    if (whiteList.includes(to.path)) {
+      next()
+    } else {
+      next('/login')
+    }
   }
-  next()
 })
 export default router
