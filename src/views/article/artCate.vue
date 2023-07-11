46adf37550404a95151a4c9af4eb76b25288c075
lyy <1528759333@qq.com>
2023-07-11 21:36:01 +0800

    30_文章分类_添加分类对话框准备

--- src/views/article/artCate.vue
+++ src/views/article/artCate.vue
@@ -3,7 +3,7 @@
     <el-card class="box-card">
       <div slot="header" class="clearfix header-box">
         <span>文章分类</span>
-        <el-button type="primary" size="mini">添加分类</el-button>
+        <el-button type="primary" size="mini" @click="addCateShowDialogFn">添加分类</el-button>
       </div>
       <!-- 分类表格标签 -->
       <el-table :data="cateList" style="width: 100%" border stripe>
@@ -20,6 +20,21 @@
         </el-table-column>
       </el-table>
     </el-card>
+    <!-- 添加文章分类对话框 -->
+    <el-dialog
+      title="提示"
+      :visible.sync="dialogVisible"
+      width="30%"
+      :before-close="handleClose"
+    >
+      <span>这是一段信息</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="cancelFn">取 消</el-button>
+        <el-button type="primary" @click="confirmFn"
+          >确 定</el-button
+        >
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -29,7 +44,8 @@ export default {
   name: 'ArtCate',
   data () {
     return {
-      cateList: []
+      cateList: [],
+      dialogVisible: false
     }
   },
   created () {
@@ -38,8 +54,23 @@ export default {
   methods: {
     async getArtCateFn () {
       const res = await getArtCateAPI()
-      console.log(res)
       this.cateList = res.data.data
+    },
+    handleClose (done) {
+      this.$confirm('确认关闭？')
+        .then(_ => {
+          done()
+        })
+        .catch(_ => {})
+    },
+    addCateShowDialogFn () {
+      this.dialogVisible = true
+    },
+    cancelFn () {
+      this.dialogVisible = false
+    },
+    confirmFn () {
+      this.dialogVisible = false
     }
   }
 }
